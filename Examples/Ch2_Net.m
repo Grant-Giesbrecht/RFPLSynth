2d75b17c4fa881c68b586aff29f256262c8f7b6a
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-03 14:01:15 -0600

    Ch2_Net: Fixed incorrect weight in error function, increased optmizer goals

--- Examples/Ch2_Net.m
+++ Examples/Ch2_Net.m
@@ -6,8 +6,6 @@ s_raw = sqrt(-1).*[18, 19, 20, 21].*1e9;
 f_scale = 21e9;
 s_vec = s_raw./f_scale;
 
-vswr_in_t = 2;
-
 % Create network object
 net = Network(4);
 net.setSPQ(SParam_Q); % Set all transistor S-parameters
@@ -16,6 +14,8 @@ net.reset();
 net.showErrors = true;
 net.vswr_in_t = 1.5;
 net.vswr_out_t = 1.5;
+net.ZL = 50;
+net.Z0 = 50;
 
 h_coef = [1,1,0];
 if force_h0
@@ -40,18 +40,23 @@ net.getStg(5).weights = [0, 0, 1];
 
 % Prepare the Levenberg-Marquardt optimization algorithm
 default_tol = 1e-6;
-options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt', 'FunctionTolerance', default_tol/100);
-options.MaxFunctionEvaluations = 6e3;
-options.MaxIterations = 4e3;
+default_iter = 400;
+default_funceval = 100*numel(net.getStg(1).h_init_guess);
+options = optimoptions('lsqcurvefit','Algorithm','levenberg-marquardt', 'FunctionTolerance', default_tol/100/100);
+% options.MaxFunctionEvaluations = 6e3;
+% options.MaxIterations = 4e3;
+options.MaxIterations = default_iter*100;
+options.MaxFunctionEvaluations = default_funceval*100;
 options.Display = 'none';
 
 net.setOptOptions(options);
 
-net.plotGain();
-
 for k=1:5
 	net.optimize(k, 'simple');
 end
+
+net.plotGain(1e9, 2);
+net.optimSummary();
 
 
 
@@ -61,7 +66,7 @@ function error_sum = error_fn1(net, k)
 
 	gain_term = stg.weights(1) * (stg.gain./stg.gain_t - 1).^2;
 	vswr_in_term = stg.weights(2) * (net.vswr_in./net.vswr_in_t - 1).^2;
-	vswr_out_term = stg.weights(2) * (net.vswr_out./net.vswr_out_t - 1).^2;
+	vswr_out_term = stg.weights(3) * (net.vswr_out./net.vswr_out_t - 1).^2;
 	
 	error_sum = sum( gain_term + vswr_in_term + vswr_out_term );
 
