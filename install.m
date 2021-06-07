1165a866fb164913f0e2dcf63b3e06e7d5f53bf9
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-06 23:14:54 -0600

    install.m: Added toolbox dependency check

--- install.m
+++ install.m
@@ -1,7 +1,9 @@
 cd src
 newpath = pwd;
 disp(['Adding to path: >>', newpath, '<<']);
-addpath(newpath);
+% addpath(newpath);
+
+successful = true;
 
 if (savepath == 0)
     disp(' ')
@@ -18,5 +20,52 @@ else
     disp('***************************************************************');
     disp('*              ERROR: Failed to update path.                  *'); 
     disp('***************************************************************');
+	
+	successful = false;
 end
 cd ..
+
+
+%=========================================================================%
+%				Check if neccesary toolboxes are installed				  %
+
+toolboxes = ["Control System Toolbox", "Optimization Toolbox", "RF Toolbox" ];
+missing = [];
+any_missing = false;
+for t = toolboxes
+	
+	if ~any(any(contains(struct2cell(ver), t)))
+		
+		if ~any_missing
+			disp(' ')
+			disp('***************************************************************');
+			disp('*         ERROR: Missing one or more required toolboxs.       *'); 
+			disp('***************************************************************');
+			any_missing = true;
+			
+			successful = false;
+		end
+		
+		missing = addTo(missing, t);
+	end
+	
+end
+
+if any_missing
+	disp(' ');
+	disp(" Missing Toolboxes:");
+	
+	for mt = missing
+		displ("    ", mt);
+	end
+end
+
+if ~successful
+	warning off backtrace
+	warning("Package not ready. Details in the messages above.");
+	warning on backtrace
+end
+
+
+
+
