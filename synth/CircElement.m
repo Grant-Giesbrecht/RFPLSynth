d275f0cd3bdea574c0713473ac9a8bb55fe1b661
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-11 15:37:19 -0600

    CircElement: Fixed initialization of 'nodes'

--- synth/CircElement.m
+++ synth/CircElement.m
@@ -13,7 +13,7 @@ classdef CircElement < handle
 	
 	methods
 		function obj = CircElement(val, unit)
-			obj.nodes = []; % pin N connects to node 'nodes(N)'
+			obj.nodes = [""]; % pin N connects to node 'nodes(N)'
 			obj.ref_type = "?"; % ex. 'R' for resistor, 'Q' for transistor
 			obj.ref_num = 0; % ex. the number in 'R4'
 			obj.val = val; % ex. the '50' in 50 ohms
