6c4ee43848eaeaf0fd87b514db599d6b72525e11
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-15 13:51:52 -0600

    Netlist.purge(): Added automatic node renaming

--- synth/Netlist.m
+++ synth/Netlist.m
@@ -18,9 +18,9 @@ classdef Netlist < handle
 		
 		function purge(obj, varargin)
 			
-			%TODO: Fix node names for removed elements (ie. n1-n2-n3,
-			%remove first element and n1 and n2 are now broken, one needs
-			%to be renamed).
+			%TODO: Fix how this works when not just ladder, removing
+			%elements should instead replace them with short, later
+			%simplify short by removing parallel, renaming, etc.
 			
 			p = inputParser;
 			p.addParameter('Zopen', 100e6, @isnumeric);
@@ -34,7 +34,8 @@ classdef Netlist < handle
 			freqs = [p.Results.f_low, p.Results.f_high];
 			
 			% Scan over all elements
-			pop_idx = [];
+			pop_idx = []; % Indeces of elements to delete
+			replacements = []; %Node to rename with other
 			idx = 0;
 			for elmt=obj.components
 				idx = idx + 1;
@@ -47,6 +48,12 @@ classdef Netlist < handle
 					% If element is open at all points, delete
 					if all( Zs > Z_open)
 						pop_idx = addTo(pop_idx, idx);
+						
+						% If brides two nodes, add to replacement list
+						if numel(elmt.nodes) > 1
+							s = struct('fnd', elmt.nodes(2:end), 'rep', elmt.nodes(1));
+							replacements = addTo(replacements, s);
+						end
 					end
 					
 				else % Else if an impedance
@@ -54,6 +61,17 @@ classdef Netlist < handle
 					% If element is shorted at all points, delete
 					if all( Zs < Z_short)
 						pop_idx = addTo(pop_idx, idx);
+						
+						% If brides two nodes, add to replacement list
+						if numel(elmt.nodes) > 1
+							idx = 2;
+							for idx = 2:numel(elmt.nodes) % Loop over all nodes (if more than 2 present)
+								s = struct('fnd', elmt.nodes(idx), 'rep', elmt.nodes(1));
+								replacements = addTo(replacements, s);
+							end
+							
+						end
+						
 					end
 					
 				end
@@ -62,6 +80,27 @@ classdef Netlist < handle
 			
 			% Delete marked elements
 			obj.components(pop_idx) = [];
+			
+			%Rename replaced nodes
+			for elmt = obj.components % For each element
+				
+				% Check each find/replacement pair
+				for r = replacements 
+					
+					% Scan over all nodes
+					for ni = 1:length(elmt.nodes)
+						
+						% If nodes match
+						if strcmp(elmt.nodes(ni), r.fnd)
+							
+							% Replace with replacement value
+							elmt.nodes(ni) = r.rep;
+						end
+						
+					end
+					
+				end
+			end
 			
 		end
 		
