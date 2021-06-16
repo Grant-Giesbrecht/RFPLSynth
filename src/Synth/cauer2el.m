36b77928bd60dbe72d1a51f296e24c8645c5087f
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-16 16:11:40 -0600

    cauer2el: Updated to use new iselement() options

--- src/Synth/cauer2el.m
+++ src/Synth/cauer2el.m
@@ -1,24 +1,24 @@
 function [k, Tn, Td] = cauer2el(num, den)
-% CAUER2EL 
+% CAUER2EL
 %
 %		   num      k       1
 %	Z(s) = ---  =  --- + ------		(Tn, Td would be passed to CAUER1 as
 %		   den      s     Tn/Td      num and den, respectively).
 %
-	
+
 	% Create symbolic polynomial from numerator and denominator
 	% coefficients
 	syms s;
 	n = poly2sym(num, s);
 	d = poly2sym(den, s);
 	Z = n/d;
-	
+
 	% Perform partial fraction decomposition
 	pf = partfrac(Z, 'FactorMode', 'Real');
-	
+
 	% Break decomposition up into terms
 	terms_cell = children(pf);
-	
+
 	% Check output type of children. In MATLAB 2020b and later it will be a
 	% cell array. Previously it will be a vector. Here the type is checked,
 	% then converted to a vector for consistency.
@@ -31,52 +31,52 @@ function [k, Tn, Td] = cauer2el(num, den)
 	else
 		terms = terms_cell;
 	end
-	
+
 	k_term = [];
 	remainder_terms = [];
-	
+
 	% Scan through each term, pick out the term(s) that follow the format
-	% of some number divided by 's'. 
+	% of some number divided by 's'.
 	for t=terms
-		
+
 		% To check order, need to pull out denomenator (as vectors)
 		[nv, dv] = sym2nd(t);
-		
+
 		% Check 't' describes a circuit element with an 's' in the
 		% denominator
-		
-		if iselement(nv, dv, 'den')
+
+		if iselement(nv, dv, 'Position', 'den', 'Format', 'Cauer')
 			k_term = t; %TODO: Replace with addTo incase there are mult num/s terms? (there shouldnt be)
 		else % Does not look like element, add to denominator
 			remainder_terms = addTo(remainder_terms, t);
 		end
-		
+
 	end
-	
+
 	% Check for missing k_term
 	if isempty(k_term)
 		error("Failed to find K value. Check input polynomial.");
 	end
-	
+
 	% Recombine all terms other than the k/s term
 	T = sum(remainder_terms);
-	
+
 	% Pull the numerator and denominator out of the sum
 	%
 	% Note: Td is getting numerator output, Tn is getting denominator. That
 	% is because T is defined as 1/(Tn/Td). It's a bit confusing, but done
 	% to make Tn feed into next stage as numerator, Td as denominator.
 	[Td, Tn] = numden(T);
-	
+
 	% Convert Td, Tn from symbolic polynomials to MATLAB polynomial vectors
 	Td = double(coeffs(Td, 'All'));
 	Tn = double(coeffs(Tn, 'All'));
-	
+
 	% Calculate K from k_term
-	[N, D] = numden(k_term); % Get numerator and denominator 
+	[N, D] = numden(k_term); % Get numerator and denominator
 	N = double(coeffs(N, 'All')); % Convert to floats
 	D = double(coeffs(D, 'All')); % COnvert to floats
 	k = N(end)/D(end-1); %K is 0-order term of numerator, divided by 1st order term of denominator
-	
-	
+
+
 end
