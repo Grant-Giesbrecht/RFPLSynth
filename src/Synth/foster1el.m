aff2220ad66d9ada228614add1cf6d7e27a81f13
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-16 16:24:16 -0600

    foster1el, foster2el: Finished new implementation of fosterXel, specified to 1 and 2
    To avoid confusion, I renamed fosterXel foster1el and made a function foster2el which simply flips the returned outputs of foster1el

--- src/Synth/foster1el.m
+++ src/Synth/foster1el.m
@@ -1,8 +1,9 @@
-function [L, C, Tn, Td] = fosterXel(num, den)
+function [L, C, Tn, Td] = foster1el(num, den)
 %
 %
-%
-%
+%          num           L*s         Tn
+%   Z(s) = ---  =  ------------- + ------      (Tn, Td would be passed to FOSTER as
+%          den      L*C*s^2 + 1       Td      num and den, respectively).
 %
 
 	% Create symbolic polynomial from numerator and denominator
@@ -31,6 +32,9 @@ function [L, C, Tn, Td] = fosterXel(num, den)
 		terms = terms_cell;
 	end
 
+	k_term = [];
+	remainder_terms = [];
+
 	% Scan through each term, pick out the term(s) that follow the format
 	% of some number divided by 's'.
 	for t=terms
@@ -51,8 +55,34 @@ function [L, C, Tn, Td] = fosterXel(num, den)
 
 	% Check for missing k_term
 	if isempty(k_term)
-		error("Failed to find K value. Check input polynomial.");
+		error("Failed to find L+C value. Check input polynomial.");
 	end
+
+	% Recombine all terms other than the k/s term
+	T = sum(remainder_terms);
+
+	% Pull the numerator and denominator out of the sum
+	[Tn, Td] = numden(T); %Note: This does NOT flip num, and den, as is required in Cauer
+
+	% Convert Td, Tn from symbolic polynomials to MATLAB polynomial vectors
+	Td = double(coeffs(Td, 'All'));
+	Tn = double(coeffs(Tn, 'All'));
+
+	% Get numerator and denominator from symbolic polynomial for Element term
+	[kn,kd] = numden(k_term);
+	el_np = Polynomial(0);
+	el_dp = Polynomial(0);
+	el__np.setVec(double(coeffs(kn, 'All')));
+	el_dp.setVec(double(coeffs(kd, 'All')));
+
+	% Get scale factor - 0th order in denominator should equal 1
+	scale_fact = el_dp.get(0);
+
+	% Get scaled coefficient from numerator's 1st order term
+	L = el_np.get(1)/scale_fact;
+
+	% Get scaled coegfficient from denominator's 2nd order term
+	C = el_dp.get(2)/scale_fact/L;
 
 %============================================ END OF NEW CODEE=================
 
---
+++ src/Synth/foster2el.m
@@ -0,0 +1,12 @@
+function [L, C, Tn, Td] = foster2el(num, den)
+%
+%
+%          num           C*s         Tn
+%   Z(s) = ---  =  ------------- + ------      (Tn, Td would be passed to FOSTER as
+%          den      L*C*s^2 + 1       Td      num and den, respectively).
+%
+
+	% Is exactly the same as foster1el, just L and C are switched
+	[C, L, Tn, Td] = foster1el(num, den);
+
+end
