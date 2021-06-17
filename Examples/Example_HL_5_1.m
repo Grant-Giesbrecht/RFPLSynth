90414177c7c5a6238f34b2a286f26264c9237a39
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-16 23:13:07 -0600

    Example_HL_5_1: Added Foster-II

--- Examples/Example_HL_5_1.m
+++ Examples/Example_HL_5_1.m
@@ -1,10 +1,44 @@
+% Create symbolic polynomial being synthesized
 syms s;
 Zin = (s^2 + 1)*(s^2 + 9) / s/(s^2 + 4)/(s^2 + 16);
 
+% Get numerator and denominator polynomial vectors
 [Zn, Zd] = sym2nd(Zin);
 
+% Create Network Synthesizer Object
 synth = NetSynth(Zn, Zd);
 
+% Generate Foster-I Network
 synth.generate('Foster1');
 
-disp(synth.circ.str());
+% Save Circuit
+foster1_cr = synth.circ;
+
+
+%Reset synthesizer
+synth.reset();
+
+% Generate Foster-II Network
+synth.generate('Foster2');
+
+% Save circuit
+foster2_cr = synth.circ;
+
+% Display Results
+displ("Foster-I Circuit:");
+disp(foster1_cr.str());
+displ(newline, "Foster-II Circuit:");
+disp(foster2_cr.str());
+
+
+
+
+
+
+
+
+
+
+
+
+
