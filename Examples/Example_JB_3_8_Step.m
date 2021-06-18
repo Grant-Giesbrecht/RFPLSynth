ee8bdce3c0f3e5c971a65367f8f500ae6731ae1b
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-18 13:48:38 -0600

    Example_JB_3_8_*: Combined section 3,8 examples

--- Examples/Example_JB_3_8_Step.m
+++ Examples/Example_JB_3_8_Step.m
@@ -1,12 +1,27 @@
-num = [100, 50, 300, 30];
+%
+%
+%
+%
+%
+%
+
+
+% Define polynomial vectors
+num_1 = [100, 50, 300, 30];
+num_2 = [18, 240, 30];
 den = [9, 170, 31, 30];
 
-synth = NetSynth(num, den);
+% Example from J+B Section 3,8,1
+synth_1 = NetSynth(num_1, den);
+synth_1.generate("Richard");
 
-% synth.richardStepZ();
-% displ("[", synth.num, "]/[", synth.den, "]");
+% Example from J+B Section 3,8,2
+synth_2 = NetSynth(num_2, den);
+synth_2.generate("Richard");
 
-synth.generate("RichardStepZ");
+% Print Results
+displ("Section 3,8,1 Example:");
+displ(synth_1.circ.str());
 
-displ("Richard's Stepped Impedance Realization:");
-displ(synth.circ.str());
+displ(newline, "Section 3,8,2 Example:");
+displ(synth_2.circ.str());
