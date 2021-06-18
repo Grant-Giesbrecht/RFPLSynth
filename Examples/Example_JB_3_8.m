93e7077ae5df43b53e94268c6002673a8c4f097b
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-18 11:53:10 -0600

    Example_JB_3_8: Added call to 'generate'

--- Examples/Example_JB_3_8.m
+++ Examples/Example_JB_3_8.m
@@ -3,4 +3,10 @@ den = [9, 170, 31, 30];
 
 synth = NetSynth(num, den);
 
-synth.richardStepZ();
+% synth.richardStepZ();
+% displ("[", synth.num, "]/[", synth.den, "]");
+
+synth.generate("RichardStepZ");
+
+displ("Richard's Stepped Impedance Realization:");
+displ(synth.circ.str());
