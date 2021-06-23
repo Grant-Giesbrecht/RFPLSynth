23cdfd9ece9ac0ee5f202842b4cfc67cc46c0b4e
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-23 11:31:18 -0400

    Zchange_high: Created example for lowerZ()

---
+++ Examples/Zchange_high.m
@@ -0,0 +1,38 @@
+synth = NetSynth([1], [1]);
+synth.freq = 1e9;
+
+tl1 = CircElement(90, "DEG");
+tl1.props("Z0") = 180;
+tl1.nodes(1) = "IN";
+tl1.nodes(2) = "n1";
+
+tl2 = CircElement(90, "DEG");
+tl2.props("Z0") = 165;
+tl2.nodes(1) = "n1";
+tl2.nodes(2) = "n2";
+
+tl3 = CircElement(45, "DEG");
+tl3.props("Z0") = 100;
+tl3.nodes(1) = "n2";
+tl3.nodes(2) = "OUT";
+synth.node_iterator = 3;
+
+synth.circ.add(tl1);
+synth.circ.add(tl2);
+synth.circ.add(tl3);
+
+synth.changeZaddStubs('Zmax', 140);
+
+% synth.raiseZ(1, 40);
+% synth.raiseZ(4, 40);
+% synth.raiseZ(7, 40);
+
+displ(synth.circ.str());
+
+synth.circ.simplify();
+
+displ(newline, synth.circ.str());
+
+synth.toStub();
+
+displ(newline, synth.circ.str());
