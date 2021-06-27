a9ec5c9770e4fa8dd9fe203ebeebc19249be21e0
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-06-27 13:59:20 -0400

    Netlist: Added doc stirngs

--- src/Synth/CircElement.m
+++ src/Synth/CircElement.m
@@ -2,6 +2,7 @@ classdef CircElement < handle
 % CIRCELEMENT Represent an element of a circuit
 %
 %	CIRCELEMENT Properties
+%		nodes - Circuit nodes to which each pin of the component connects
 %		ref_type - Letter used in reference identifier (eg. 'R' for 'R1')
 %		ref_num - Reference number (eg. '1' in 'R1')
 %		val - Component value
@@ -11,20 +12,20 @@ classdef CircElement < handle
 %		unique_id - Circuit number, unique among circuit elements in a
 %		netlist for easy identification of components.
 
-	
+
 	properties
 		nodes
-		
+
 		ref_type;
 		ref_num;
 		val;
 		val_unit;
 		part_no;
 		props;
-		
+
 		unique_id;
 	end
-	
+
 	methods
 		function obj = CircElement(val, unit)
 		%CIRCELEMENT Initialize the CIRCELEMENT object
@@ -32,7 +33,7 @@ classdef CircElement < handle
 		%	obj = CIRCELEMENT(val, unit) Create a circuit element with
 		%	value 'val' with units 'unit'.
 		%
-		
+
 			obj.nodes = [""]; % pin N connects to node 'nodes(N)'
 			obj.ref_type = "?"; % ex. 'R' for resistor, 'Q' for transistor
 			obj.ref_num = 0; % ex. the number in 'R4'
@@ -40,37 +41,37 @@ classdef CircElement < handle
 			obj.val_unit = unit; % ex. the 'ohms' in 50 ohms
 			obj.part_no = ""; % Part number/identifier. Can be for physical part number in manufacturing, or to distiguish type of element
 			obj.props = containers.Map; % Other properties (ex. for transmission lines)
-			
+
 			obj.format();
-			
+
 			obj.unique_id = -1;
 		end
-		
+
 		function s = str(obj)
 		%STR Display the object as a string
 		%
 		%	s = STR() Create a string containing the circuit element's
 		%	data.
-			
+
 			% Create node string
 			nodestr = "";
 			for n=obj.nodes
-				
+
 				% Add junction if not first element
 				if ~strcmp(nodestr, "")
 					nodestr = strcat(nodestr, "-");
 				end
-				
+
 				nodestr = strcat(nodestr, n);
 			end
-			
+
 			scaled_str = scaleNumUnit(obj.val, obj.val_unit);
-			
+
 			s = strcat(scaled_str, "   (", nodestr, ")");
-			
+
 			if strcmp(obj.ref_type, "TL") && isKey(obj.props, "Z0")
 				s = strcat(s, "  [Z0 = ", string(obj.props("Z0")), " Ohms, ");
-				
+
 				if isKey(obj.props, "Stub") && islogical(obj.props("Stub")) && obj.props("Stub")
 					if isKey(obj.props, "Term") && strcmp(obj.props("Term"), "SHORT")
 						s = strcat(s, "Shorted Stub]");
@@ -80,31 +81,31 @@ classdef CircElement < handle
 				else
 					s = strcat(s, "TLine]");
 				end
-				
+
 			end
-			
+
 		end
-		
+
 		function format(obj) %============== format() =====================
 		% FORMAT Populate the object's data fields inferring from val_unit.
 		%
-		% Uses val_unit to infer the component's type. 
+		% Uses val_unit to infer the component's type.
 		%
 		%	FORMAT() Populate the object's data fields
 		%
-		
+
 			ustr = string(obj.val_unit);
 			cu = char(obj.val_unit);
-			
+
 			if length(ustr) > 1
 				[mult, baseUnit] = parseUnit(ustr);
 			else
 				mult = 1;
 				baseUnit = ustr;
 			end
-			
+
 			baseUnit = upper(baseUnit);
-			
+
 			% Handle many units of electical length in the switch below
 			if strcmp(baseUnit, "DEGREE") || strcmp(baseUnit, "DEGREES")
 				baseUnit = "DEG";
@@ -112,7 +113,7 @@ classdef CircElement < handle
 			if strcmp(baseUnit, "RADIAN") || strcmp(baseUnit, "RADIANS")
 				baseUnit = "DEG";
 			end
-			
+
 			switch baseUnit
 				case "F"	% Capacitors
 					obj.ref_type = "C";
@@ -128,20 +129,20 @@ classdef CircElement < handle
 					obj.ref_type = "TL";
 				case "RAD"	% Transmission Lines
 					obj.ref_type = "TL";
-				otherwise	% Otherwise assume unit is type (ex. transistor 
+				otherwise	% Otherwise assume unit is type (ex. transistor
 					obj.ref_type = obj.val_unit;
 			end
-			
+
 		end %==================== END format() ============================
-		
+
 		function zf = Z(obj, freq) %===================== Z() =============
 		% Z Calculate the component's impedance
 		%
 		% zf = Z(freq) Calcualtes the object's impedance at frequency
 		% 'freq'. This function is only valid for inductors, capacitors,
-		% and resistors with ref_types 'L', 'C', and 'R', respectively. 
+		% and resistors with ref_types 'L', 'C', and 'R', respectively.
 		%
-		
+
 			switch obj.ref_type
 				case "C"
 					zf = Zc(obj.val, freq);
@@ -152,8 +153,8 @@ classdef CircElement < handle
 				otherwise
 					zf = NaN;
 			end
-					
+
 		end %========================== END Z =============================
 	end
-	
+
 end
--- src/Synth/Netlist.m
+++ src/Synth/Netlist.m
@@ -1,9 +1,21 @@
 classdef Netlist < handle
+% NETLIST Represents a circuit as a netlist
+%
+% Represents a circuit or network as a selection of CircElement objects, with
+% connections indicated via their node terminations.
+%
+% NETLIST Properties
+%	components - List of CircElements that comprise the Netlist
+%	next_unique_id - Next value to assign to a new component's unique_id added
+%	the netlist.
+%	ref_ids - Map of reference numbers, organzied by ref_type as the maps keys
+%	so annotation doesn't produce duplicate references.
+%
 
 	properties
 
 		components
-		
+
 		next_unique_id
 
 		ref_ids
@@ -11,22 +23,30 @@ classdef Netlist < handle
 
 	methods
 		function obj = Netlist(comp)
+		% NETLIST Initializer for Netlist object
+		%
+		%	obj = NETLIST(comp) Create a netlist object and initialize the
+		%	component list with 'comp'
+		%
+
 			obj.components = comp;
-			
+
 			obj.next_unique_id = 1;
-			
+
 			obj.ref_ids = containers.Map();
 		end
 
 		function tf = addAt(obj, elmnt, idx)
+		% ADDAT Add a CircElement to the Netlist at the specified index
 		%
-		% components = [A, B, C, D]
-		% addTo(X, 3)
-		% components = [A, B, X, C, D];
+		%	tf = ADDAT(elmnt, idx) Add a CircElement at the index 'idx'. Every
+		%	CircElement that was at index 'idx' or higher will be shifted
+		%	backwards by 1 index. Returns true if successful.
 		%
-			
+		% See also: Add
+
 			tf = true;
-			
+
 			% Verify that correct type is being added
 			if ~isa(elmnt, 'CircElement')
 				warning("Failed to add element. Incorrect type (expected CircElement)");
@@ -37,90 +57,109 @@ classdef Netlist < handle
 			% Assign a unique ID to the element
 			elmnt.unique_id = obj.next_unique_id;
 			obj.next_unique_id = obj.next_unique_id + 1;
-			
+
 			obj.components = [obj.components(1:idx-1), elmnt, obj.components(idx:end)];
-			
+
 		end
-		
+
 		function tf = add(obj, elmnt)
+		% ADD Add a CircElement at the back of the Netlist components list
+		%
+		%	tf = ADD(elmnt) Add a CircElement 'elmnt' to the back of the
+		%	component list. Returns true if sucessful.
+		%
+		% See also: addTo
 
 			obj.addAt(elmnt, length(obj.components)+1);
-			
+
 		end
-		
+
 		function idx = ID2Idx(obj, ID)
-			
+		% ID2IDX Looks up a component by unique_id and returns its index
+		%
+		%	idx = ID2IDX(ID) Finds the CircElement with unique_id 'ID' and
+		%	returns its ID in 'components'.
+
 			index = 0;
 			for e = obj.components
-				
+
 				index = index + 1;
-				
+
 				if e.unique_id == ID
 					idx = index;
 					return;
 				end
-				
+
 			end
-			
+
 		end
 
 		function tf = simplify(obj)
+		% SIMPLIFY Simplifies the circuit's structure
 		%
+		% Simplifies the circuit's structure by:
+		%	1. Combining parallel elements of same type
+		%	2. Combining series elements of same type
 		%
-		%	* purge shorts
-		%   * Combine parallel elements
-		%	* Combine shunt elements
+		%	tf = SIMPLIFY() Simplify the circuit structure.
 		%
-		
+		% See also: purge
+
 			%TODO: Fix this so it works with any and all topologies, not
 			%just ladder
-			
-			
-			
+
+
+
 			% Combine serial and parallel components until no more
 			% simplifications can be mades
 			modified = true;
 			while modified
-				
+
 				% Combine parallel components
 				modified = obj.pallCombine();
-				
+
 				% Combine serial components
 				modified = modified || obj.serCombine();
-				
+
 			end
-			
-			
-		
+
+
+
 		end
-		
+
 		function mod = pallCombine(obj)
-			
+		% PALLCOMBINE Combine parallel elements in the Netlist
+		%
+		%	mod = PALLCOMBINE() Combines parallel elements of the same type.
+		%	Returns true if the function modified the Netlist.
+		%
+		% See also: serCombine, simplify
+
 			del_idx = [];
 			mod = false;
-			
+
 			% Combine parallel elements
 			ei = 0;
 			while ei < length(obj.components)-1 % Loop over each element
-				
+
 				% Increment counter
 				ei = ei + 1;
-				
+
 				% Skip if first node does not match
 				if obj.components(ei).nodes(1) ~= obj.components(ei+1).nodes(1)
 					continue;
 				end
-				
+
 				% Skip if 2nd node does not match
 				if obj.components(ei).nodes(2) ~= obj.components(ei+1).nodes(2)
 					continue;
 				end
-				
+
 				% Skip if not same type of component
 				if ~strcmp(obj.components(ei).ref_type, obj.components(ei+1).ref_type)
 					continue;
 				end
-				
+
 				% Combine elements
 				if strcmp(obj.components(ei).ref_type, "C")
 					obj.components(ei).val = obj.components(ei).val + obj.components(ei+1).val;
@@ -129,43 +168,49 @@ classdef Netlist < handle
 				else
 					continue;
 				end
-				
+
 				mod = true;
-				
+
 				% Mark second for deletion
 				del_idx = addTo(del_idx, ei+1);
-				
+
 				% Increment counter again if elements combined
 				ei = ei + 1;
 			end
-			
+
 			% Delete combined components
 			obj.components(del_idx) = [];
-			
+
 		end
-		
+
 		function mod = serCombine(obj)
-			
+		% SERCOMBINE Combine series elements in the Netlist
+		%
+		%	mod = SERCOMBINE() Combines series elements of the same type.
+		%	Returns true if the function modified the Netlist.
+		%
+		% See also: pallCombine, simplify
+
 			del_idx = [];
 			mod = false;
-			
+
 			% Combine series elements
 			ei = 0;
 			while ei < length(obj.components)-1 % Loop over each element
-				
+
 				% Increment counter
 				ei = ei + 1;
-				
+
 				% Skip if elements dont share any nodes
 				if ~isSeries(obj.components(ei), obj.components(ei+1))
 					continue;
 				end
-				
+
 				% Skip if not same type of component
 				if ~strcmp(obj.components(ei).ref_type, obj.components(ei+1).ref_type)
 					continue;
 				end
-				
+
 				% Combine elements
 				if strcmp(obj.components(ei).ref_type, "C")
 					obj.components(ei).val = pall( obj.components(ei).val, obj.components(ei+1).val);
@@ -174,21 +219,37 @@ classdef Netlist < handle
 				else
 					continue;
 				end
-				
+
 				mod = true;
-				
+
 				% Mark second for deletion
 				del_idx = addTo(del_idx, ei+1);
-				
+
 				% Increment counter again if elements combined
 				ei = ei + 1;
 			end
-			
+
 			% Delete combined components
 			obj.components(del_idx) = [];
 		end
-		
+
 		function purge(obj, varargin)
+		% PURGE Purges negligable components from the Netlist
+		%
+		% Removes CircElements from the netlist that are series shorts or
+		% parallel opens. Currently only recognizes elements as parallel if
+		% terminating in GND on one side.
+		%
+		%	PURGE(..., options) Purges negligable components.
+		%
+		% OPTIONS:
+		%	'Zopen' - Numeric value to classify as open circuit (default: 100e6)
+		%	'Zshort' - Numeric value to classify as a short circuit (default:
+		%	1e-3).
+		%	'f_low' - Minimum frequency at which to check compponent values
+		%	'f_high' - Maximum frequency at which to check component values
+		%
+		% See also: simplify
 
 			%TODO: Fix how this works when not just ladder, removing
 			%elements should instead replace them with short, later
@@ -275,55 +336,70 @@ classdef Netlist < handle
 			end
 
 		end
-		
+
 		function annotate(obj, reset)
-			
+		% ANNOTATE Add reference numbers to all components in the Netlist
+		%
+		%	ANNOTATE() Annotate all components without reference numbers (ie.
+		%	ref_num == 0).
+		%
+		%	ANNOTATE(reset) Annotate all coponents and reset existing annotation
+		%	if 'reset' is set to true.
+		%
+
 			% Check if 'reset' exists
 			if ~exist('reset', 'var')
 				reset = false;
 			end
-			
+
 			% Reset annotations if asked
 			if reset
 				for c=obj.components
 					c.ref_num = 0;
 				end
 			end
-			
+
 			% For each component...
 			for c=obj.components
-				
+
 				% Skip labeled components
 				if c.ref_num ~= 0
 					continue;
 				end
-				
+
 				% Otherwise check if key exists
 				if isKey(obj.ref_ids, c.ref_type)
-					
+
 					% Get new ID
 					c.ref_num = Netlist.nextListID(obj.ref_ids(c.ref_type));
-					
+
 					% Add ID to list
 					obj.ref_ids(c.ref_type) = addTo(obj.ref_ids(c.ref_type), c.ref_num);
 				else
-					
+
 					% Assign component as '1'
 					c.ref_num = 1;
-					
+
 					% Add to ref_ids
 					obj.ref_ids(c.ref_type) = 1;
 				end
-				
+
 			end
-			
+
 		end %========================= END annotate() =====================
 
 		function s = str(obj, indent)
+		% STR Generate a string representing the Netlist
+		%
+		%	s = STR() Generate Netlist string
+		%
+		%	s = STR(indent) Generate a Netlist string, with every line preceeded
+		%	by the string 'indent'.
+		%
 
 			simple_output = false;
 			precision = 3;
-			
+
 			if ~exist('indent', 'var')
 				indent = "    ";
 			end
@@ -343,25 +419,25 @@ classdef Netlist < handle
 
 				end
 			else
-				
+
 				obj.annotate();
-				
+
 				% Initialize table
 				mt = MTable();
 				mt.table_title("Netlist");
 				mt.row(["Ref.", "Value", "Node 1", "Node 2", "Z0", "Stub Type"]);
-				
+
 				rowstr = "";
-				
+
 				for c=obj.components
-					
+
 					rowstr = strcat(c.ref_type, string(c.ref_num));
 					rowstr(2) = strcat(MTable.prd(c.val, precision), " ", c.val_unit);
 					rowstr(3) = c.nodes(1);
 					rowstr(4) = c.nodes(2);
 					rowstr(5) = " - ";
 					rowstr(6) = " - ";
-					
+
 					% Handle TL specific fields
 					if c.ref_type == "TL"
 						if isKey(c.props, "Z0")
@@ -379,36 +455,45 @@ classdef Netlist < handle
 							end
 						end
 					end
-					
+
 					mt.row(rowstr);
-					
+
 				end
-				
+
 				mt.alignac('r');
-				
+
 				s = mt.str();
-				
+
 			end
-			
-			
+
+
 		end
 
 	end
-	
+
 	methods (Static)
-		
+
 		function id = nextListID(list, quick)
-			
+		% NEXTLISTID Get the next ref_id by checking which values are used
+		%
+		%	id = NEXTLISTID() Find the next ref_id by finding the lowest unused
+		%	integer for all CircElements of one type (as speficied by ref_type).
+		%
+		%	if = NEXTLISTID(quick) If 'quick' is true, find the next ref_id by
+		%	adding one to the highest ref_id for all CircElements of one type.
+		%	Otherwise, call is identical to NEXTLISTID().
+		%
+
 			if ~exist('quick', 'var')
 				quick = false;
 			end
-			
+
 			% Make sure list is numeric
 			if ~isnumeric(list)
 				id = NaN;
 				return;
 			end
-			
+
 			if ~quick
 				id = 1;
 				while any(id == list)
@@ -418,7 +503,7 @@ classdef Netlist < handle
 				id = ceil(max(list))+1;
 			end
 		end
-		
+
 	end
 
 end
